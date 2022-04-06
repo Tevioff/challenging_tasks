357537e0661629e8c7815c7cf3b52c756fe6c4c4
Təvəkkül <tevizeynalov@gmail.com>
2022-04-06 13:49:26 +0400

    сложное задание девятого дня

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
 'use strict';
 
-let arr = ['22', '37', '400', '311', '49', '81', '247'];
-
-for (let i = 0; i < 7; i++) {
-    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-        console.log(arr[i]);
-    }
-}
-let num;
-for (let i = 2; i <= 100; i++) {
-    let simple = 1;
-    for (let j = 2;
-        (j <= i / 2) && (simple == 1); j = j + 1) {
-        if (i % j == 0) {
-            simple = 0;
+setInterval(() => {
+    function datenAndTime(value) {
+        if (value < 10) {
+            value = '0' + value;
         }
+        return value;
     }
-    if (simple == 1) {
-        console.log('простое: ' + i + ' ' + ' Делители этого числа: 1 и ' + i);
-        num += 'простое: ' + i + ' ' + ' Делители этого числа: 1 и ' + i + '<br>';
+
+    function dateTime() {
+        const current_datetime = new Date();
+        const day = datenAndTime(current_datetime.getDate());
+        const month = datenAndTime(current_datetime.getMonth() + 1);
+        const year = current_datetime.getFullYear();
+        const hours = datenAndTime(current_datetime.getHours());
+        const minutes = datenAndTime(current_datetime.getMinutes());
+        const seconds = datenAndTime(current_datetime.getSeconds());
+
+        return day + "." + month + "." + year + " - " + hours + ":" + minutes + ":" + seconds;
     }
-}
+
+    document.getElementById('time').innerHTML = dateTime();
+}, 1000);
+
+
+
+
+setInterval(() => {
+    function time() {
+        const week = [
+            'Воскресенье',
+            'Понедельник',
+            'Вторник',
+            'Среда',
+            'Четверг',
+            'Пятница',
+            'Суббота'
+        ];
+        
+        const month = [
+            'Января',
+            'Февраля',
+            'Марта',
+            'Апреля',
+            'Майа',
+            'Июня',
+            'Июля',
+            'Августа',
+            'Сентября',
+            'Октября',
+            'Ноября',
+            'Декабря',
+        ];
+        function dateAndTime(value) {
+            if (value < 10) {
+                value = '0' + value;
+            }
+            return value;
+        }
+        const d = new Date();
+        const n = d.getDay();
+        const mon = d.getMonth();
+        const days = d.getDay();
+        const years = d.getFullYear();
+        const hours = dateAndTime(d.getHours());
+        const minutes = dateAndTime(d.getMinutes());
+        const seconds = dateAndTime(d.getSeconds());
+
+
+        return `Сегодня ${week[n]}, ${days} ${month[mon]} ${years} года, ${hours} часов ${minutes} минут ${seconds} секунд `;
+
+    }
+    document.getElementById('fullTime').innerHTML = time();
+}, 1000);
