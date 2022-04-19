1435da3221f6d45743b8f4e299614e6f868e39b7
Təvəkkül <tevizeynalov@gmail.com>
2022-04-20 00:07:04 +0400

    feat

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const input = document.getElementById('input');
 const p = document.getElementById('p');
 
-input.addEventListener('input', updateValue);
+const print = () => {
+	p.textContent = '';
+	p.textContent += input.value;
+};
 
-function updateValue(e) {
-    p.textContent = e.target.value;
-}
+const debounce = function (f, t) {
+	return function () {
+		let previousCall = this.lastCall;
+		this.lastCall = Date.now();
+
+		if (previousCall && ((this.lastCall - previousCall) <= t)) {
+			clearTimeout(this.lastCallTimer);
+		}
+		this.lastCallTimer = setTimeout(f, t);
+	};
+};
+
+input.addEventListener('input', debounce(print, 300));
