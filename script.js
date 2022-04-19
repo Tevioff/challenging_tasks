3aca95b33f3b56ad402a65a86a7fdb709d72f94e
Təvəkkül <tevizeynalov@gmail.com>
2022-04-19 22:40:34 +0400

    animation

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const input = document.getElementById('input');
-const p = document.getElementById('p');
+const car = document.getElementById('car');
+const car1 = document.getElementById('car1');
 
-input.addEventListener('input', updateValue);
+let count = 0;
+let idInterval;
+let active = true;
 
-function updateValue(e) {
-    p.textContent = e.target.value;
-}
+const speedAnimation = () => {
+	count++;
+	idInterval = requestAnimationFrame(speedAnimation);
+	if (count < 710) {
+		car.style.left = count * 3.4 + 'px';
+		car1.style.left = count * 3 + 'px';
+	} else {
+		cancelAnimationFrame(idInterval);
+	}
+};
+speedAnimation();
+document.addEventListener('click', () => {
+	if(active) {
+		cancelAnimationFrame(idInterval);
+		active = false;
+	} else {
+		idInterval = requestAnimationFrame(speedAnimation);
+		active = true;
+	}
+});
